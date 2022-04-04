7230b2215a72b6ef351aafda05f7a2292da681ec
Rbon <robinbisho@gmail.com>
2022-04-03 23:13:07 -0700

    Basically a full rewrite

--- LibRbon.hs
+++ LibRbon.hs
@@ -2,6 +2,24 @@ module LibRbon where
 
 import qualified Data.Text as T
 
+import System.IO ( stdout, hFlush )
+
+prompt :: String -> IO String
+prompt text = do
+    putStr text
+    hFlush stdout
+    getLine
+
+notEquals :: Eq a => a -> a -> Bool
+notEquals = (/=)
+
+remove :: Eq a => [a] -> a -> [a]
+remove = flip $ filter . (/=)
+
+(!=) :: Eq a => a -> a -> Bool
+(!=) = (/=)
+infixl 1 !=
+
 (|>) :: a -> (a -> b) -> b
 (|>) = flip ($)
 infixl 1 |>
@@ -14,9 +32,26 @@ infixl 1 ||>
 (|->) = flip (.)
 infixl 1 |->
 
-(|=>) :: (a -> (b, c)) -> (b -> c -> d) -> a -> d
-(|=>) f g = f |-> uncurry g
+(<-|) :: (b -> c) -> (a -> b) -> a -> c
+(<-|) = (.)
+
+(|=>) :: (a -> b -> c) -> (c -> d) -> a -> b -> d
+(|=>) = flip (...)
 infixl 1 |=>
+
 
 splitOnString :: String -> String -> [String]
 splitOnString delim = T.pack |-> T.splitOn (T.pack delim) |-> map T.unpack
+
+-- | Compose two functions.
+-- except that @g@ will be fed /two/ arguments instead of one
+-- before handing its result to @f@.
+(...) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
+(f ... g) x y = f (g x y)
+infixr 8 ...
+
+
+-- think of this as a pipe that feeds the right function two arguments
+-- (<=|) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
+-- (<=|) = (.) . (.)
+-- infixl 1 <=|
--- wordle_solver.hs
+++ wordle_solver.hs
@@ -1,210 +1,170 @@
 import LibRbon
--- import Words as W
-import Text.Printf
+import Text.Printf ()
+import Data.Function (on)
+import Data.Sequence (takeWhileR, index)
+import GHC.Exts.Heap (GenClosure(what_next))
 
--- what is this I never typed this
--- import Distribution.PackageDescription (defaultIncludeRenaming)
+data WordleLetter = WordleLetter {
+    char     :: Char       ,
+    inOneOf  :: Maybe [Int],
+    mustBeIn :: Maybe Int  ,
+    cantBeIn :: Maybe Int  }
+    deriving (Show)
 
-type GuessLetter = (Char, Char, Int)
-
-type Guess = [GuessLetter]
-
-type Board = [Guess] 
-
-type PossibleWord = String
-
-type Bad = Char
-
-type Unknown = Int
-
-type Green = WordleLetter
-
-type Yellow = WordleLetter
-
-data State = State {
-    wordList    :: [PossibleWord],
-    greens   :: [Green],
-    yellows  :: [Yellow],
-    bads     :: [Bad],
-    unknowns :: [Unknown],
-    board    :: Board
-    } deriving (Show)
-
-type WordleLetter = (Char, Char, Int)
-
--- main = do
---     let x = generateWords "abcdef"
---     print x
-
-generateWords :: State -> State
-generateWords state = do
-    let alphabet = "abcdefghijklmnopqrstuvwxyz"
-    let chars = alphabet |> filter (`notElem` bads state)
-    let words = sequence [chars, chars, chars, chars, chars]
-    State words (greens state) (yellows state) (bads state) (unknowns state) (board state)
-
-fitsAllGreens :: String -> [Green] -> Bool
-fitsAllGreens = do
-    let fitsGreen word (c, _, i) = atIndex word c i
-    fitsGreen |-> all
-
-fitsYellow :: String -> [Int] -> WordleLetter -> Bool
-fitsYellow word unknowns (char, _, pos) =
-    not $ atIndex word char pos && atAnyIndex word char unknowns
-
-fitsAllYellows :: String -> [Int] -> [WordleLetter] -> Bool
-fitsAllYellows = curry $ all . uncurry fitsYellow
-
-atAnyIndex :: String -> Char -> [Int] -> Bool
-atAnyIndex = curry $ any . uncurry atIndex
-
-atIndex :: String -> Char -> Int -> Bool
-atIndex word c pos = word !! pos == c
-
-lettersInWord ::String -> [Char] -> Bool 
-lettersInWord = flip elem |-> any
-
-checkWord :: [WordleLetter]
-          -> [WordleLetter]
-          -> [Int]
-          -> [Char]
-          -> String
-          -> Bool
-checkWord greens yellows unknown bads word = do
-    let check1 = not $ lettersInWord word bads
-    let check2 = fitsAllGreens word greens
-    let check3 = fitsAllYellows word unknown yellows
-    check1 && check2 && check3
-
-filterWords :: State -> State
-filterWords (State words greens yellows unknown bads board) = do
-    let new_words = filter (checkWord greens yellows bads unknown) words
-    State new_words greens yellows unknown bads board
-
-guess :: String -> Guess
-guess = do
-    let addNumbers n (char, color) = (char, color, n)
-    splitOnString " = "
-        |-> (\[x, y] -> (x,y))
-        |=> zip
-        |-> zipWith addNumbers [0..4]
-
-parse :: String -> State
-parse command = do
-    let letters = command |> guess
-    let greens = letters |> filter (\(_, c, _) -> c == 'g')
-    let yellows = letters |> filter (\(_, c, _) -> c == 'y')
-    let bads = letters
-               |> filter (\(_, c, _) -> c == 'b')
-               |> map (\(c, _, _) -> c)
-    State [] greens yellows bads [] [letters]
-
-updateState :: State -> State -> State
-updateState (State w1 g1 y1 ba1 u1 bo1) (State w2 g2 y2 ba2 u2 bo2) = do
-    let words = w1 ++ w2
-    let greens = g1 ++ g2
-    let yellows = y1 ++ y2
-    let bads = ba1 ++ ba2
-    let unknowns = (g1, g2) ||> determineUnknown
-    let board = bo1 ++ bo2
-    State words greens yellows bads unknowns board
-
-determineUnknown :: [Green] -> [Green] -> [Unknown]
-determineUnknown g1 g2 = do
-    let u = [0..4]
-    let greenIndexes1 = g1 |> map (\(_, _, x) -> x)
-    let greenIndexes2 = g2 |> map (\(_, _, x) -> x)
-    let totalGreenIndexes = greenIndexes1 ++ greenIndexes2
-    u |> filter (`elem` totalGreenIndexes)
-
-
-step :: String -> State -> State
-step command state = do
-    let new_state = command |> parse
-    let state2 = updateState state new_state
-                 |> generateWords
-                 |> filterWords
-    State
-        (wordList state2)
-        (greens state2)
-        (yellows state2)
-        (bads state2)
-        (unknowns state2)
-        (board state2)
-
-fullStep :: IO State
-fullStep = do
-    command <- getLine
-    let emptyState = State [] [] [] [] [] []
-    print "Working..."
-    let state = step command emptyState
-    print $ fancyPrint state
-    return state
-
-fancyPrint :: State -> String
-fancyPrint state = printf "Total words: %d" (length (wordList state))
+type State = ([WordleLetter], [String])
 
 main :: IO ()
-main = do
-    state <- fullStep
-    main
+main = interactiveStep []
 
---     if state.words |> tuple |> len == 0:
---         case cheat_level:   
---             match 0:
---                 "generating words sans bad letters..." |> print
---                 # start_time = time.perf_counter()
---                 words = generate_words(state.bads)
---                 # end_time = time.perf_counter()
---                 # "finished in: " + (end_time - start_time |> str) |> print
---                 'total words: ' + (words |> len |> str) + '\n' |> print
---             match 1:
---                 words = wordle_words.all_words
---             match 2:
---                 words = wordle_words.possible_solutions
---     else:
---         words = state.words
+interactiveStep :: [String] -> IO ()
+interactiveStep wordList = do
+    command <- prompt "guess: "
+    let newWordList = step command wordList
+    fancyPrint newWordList
+    interactiveStep newWordList
 
+fancyPrint :: [String] -> IO ()
+fancyPrint wordList
+    | length wordList > 99 = print (length wordList)
+    | otherwise            = print wordList
 
--- def fancy_print(state: State, word_max: int = 1000):
---     words = state.words |> fancy_format
---     if state.words |> len > word_max:
---         print("Too many words to print.")
---     else:
---         print(words)
---     print("Total words: " + str(len(state.words)))
---     print()
---     print("Board: \n" + color_board(state.board))
+initStep :: String -> [String]
+initStep command = step command (generateWords $ commandToLetters command)
 
--- def color_letter(letter: Letter) -> str:
---     case letter[1]:
---         match 'g':
---             ouput = f"{bcolors.GREEN}{letter[0]}{bcolors.ENDC}"
---         match 'y':
---             output = f"{bcolors.YELLOW}{letter[0]}{bcolors.ENDC}"
---         match 'b':
---             output = f"{bcolors.BLACK}{letter[0]}{bcolors.ENDC}"
---     return output
+step :: String -> [String] -> [String]
+step command []       = initStep command
+step command wordList = filterWords (commandToLetters command) wordList
 
--- color_board = fmap$(fmap$(color_letter) ..> join $ ('')) ..> join $ ('\n')
+initWords :: String -> [String]
+initWords = generateWords . commandToLetters
 
--- def main():
---     state = State()
---     # thing.strip() |> print
---     while True:
---         print()
---         i = input("yes?: ")
---         state = (i, state) |*> step$(cheat_level=1)
---         state |> fancy_print
+commandToLetters :: String -> [WordleLetter]
+commandToLetters command =
+    let pairs = uncurry zip . (\[x, y] -> (x,y)) . splitOnString " = "
+        indexedPairs = insertIndex $ pairs command
+        unknowns = pairsToUnknowns indexedPairs
+        yellows = generateYellows indexedPairs unknowns
+        greens = generateGreens indexedPairs
+        wrongs = generateWrongs indexedPairs
+    in  greens ++ yellows ++ wrongs
 
--- def color_green(text) = f"{bcolors.GREEN}{text}{bcolors.ENDC}"
+generateGreens :: [(Char, Char, Int)] -> [WordleLetter]
+generateGreens indexedPairs =
+    let pred (_, color, _) = color == 'g'
+        filteredPairs = filter pred indexedPairs
+        f (char, _, index) =
+            WordleLetter {
+                char = char,
+                cantBeIn = Nothing,
+                mustBeIn = Just index,
+                inOneOf  = Nothing}
+    in  map f filteredPairs
 
--- def color_yellow(text) = f"{bcolors.YELLOW}{text}{bcolors.ENDC}"
+generateYellows :: [(Char, Char, Int)] -> [Int] -> [WordleLetter]
+generateYellows indexedPairs unknowns =
+    let pred (_, color, _) = color == 'y'
+        filteredPairs = filter pred indexedPairs
+        realUnknowns = unknowns
+        f (char, color, index) =
+            WordleLetter {
+                char = char,
+                cantBeIn = Just index,
+                inOneOf  = Just (remove unknowns index),
+                mustBeIn = Nothing}
+    in  map f filteredPairs
 
--- def color_black(text) = f"{bcolors.BLACK}{text}{bcolors.ENDC}"
+generateWrongs :: [(Char, Char, Int)] -> [WordleLetter]
+generateWrongs indexedPairs =
+    let pred (_, color, _) = color == 'w'
+        filteredPairs = filter pred indexedPairs
+        f (char, _, index) =
+            WordleLetter {
+                char     = char,
+                cantBeIn = Just index,
+                mustBeIn = Nothing,
+                inOneOf  = Nothing}
+    in  map f filteredPairs
 
--- class bcolors:
---     GREEN = '\033[97;42m'
---     YELLOW = '\033[97;43m'
---     BLACK = '\033[97;100m'
---     ENDC = '\033[0m'
+pairsToUnknowns :: [(Char, Char, Int)] -> [Int]
+pairsToUnknowns =
+    filter (\(_, color, _) -> color /= 'g')
+    |-> map (\(_, _, i) -> i)
+
+insertIndex :: [(Char, Char)] -> [(Char, Char, Int)]
+insertIndex pairs =
+    let f (x, y) n = (x, y, n)
+        indexes = map (flip (-) 1) [1 .. length pairs]
+    in  zipWith f pairs indexes
+
+isPossibleWord :: [WordleLetter] -> String -> Bool
+isPossibleWord ls s =
+    let atLeastOneElem c s xs = any ((== c) . (s !!)) xs
+        matchOneOf s c Nothing   = True 
+        matchOneOf s c (Just xs) = atLeastOneElem c s xs
+        matchCant  s c Nothing   = True
+        matchCant  s c (Just i)  = s !! i /= c
+        matchMust  s c Nothing   = True
+        matchMust  s c (Just i)  = s !! i == c
+        f s l =
+            matchOneOf s (char l) (inOneOf l)
+            && matchCant s (char l) (cantBeIn l)
+            && matchMust s (char l) (mustBeIn l)
+    in  all (f s) ls
+
+filterWords :: [WordleLetter] -> [String] -> [String]
+filterWords = filter . isPossibleWord
+
+pairs :: String -> [(Char, Char)]
+pairs = uncurry zip . (\[x, y] -> (x,y)) . splitOnString " = "
+
+generateWords :: [WordleLetter] -> [String]
+generateWords letters = sequence $ f letters where
+    f letters = filterPossibleChars letters initPossibles
+
+filterPossibleChars :: [WordleLetter] -> [String] -> [String]
+filterPossibleChars letters = filterCants cants . filterMusts musts where
+    musts = collectMusts letters
+    cants = collectCants letters
+
+filterMusts:: [[Char]] -> [String] -> [String]
+filterMusts = zipWith f where
+    f "" str = str
+    f xs str = xs
+
+filterCants :: [[Char]] -> [String] -> [String]
+filterCants = zipWith f where
+    f "" str = str
+    f xs str = foldl remove str xs
+
+collectMusts :: [WordleLetter] -> [[Char]]
+collectMusts = foldl1 (zipWith (++)) . map h where
+    initList = [[], [], [], [], []]
+    f xs c i = replace xs (i, [c])
+    g xs c Nothing  = xs
+    g xs c (Just i) = f xs c i
+    h letter = g initList (char letter) (mustBeIn letter)
+
+collectCants :: [WordleLetter] -> [[Char]]
+collectCants = foldl1 (zipWith (++)) . map h where
+    h letter = g initList (char letter) (cantBeIn letter)
+    g xs c Nothing  = xs
+    g xs c (Just i) = f xs c i
+    f xs c i = replace xs (i, [c])
+    initList = [[], [], [], [], []]
+
+-- stolen from stackoverflow
+replace :: (Num a, Ord a) => [b] -> (a, b) -> [b]
+replace [] _ = []
+replace (_:xs) (0,a) = a:xs
+replace (x:xs) (n,a) =
+  if n < 0
+    then x:xs
+    else x: replace xs (n-1,a)
+
+initPossibles :: [String]
+initPossibles = [
+    "abcdefghijklmnopqrstuvwxyz" ,
+    "abcdefghijklmnopqrstuvwxyz" ,
+    "abcdefghijklmnopqrstuvwxyz" ,
+    "abcdefghijklmnopqrstuvwxyz" ,
+    "abcdefghijklmnopqrstuvwxyz" ]
