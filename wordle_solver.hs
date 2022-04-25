4f1b771b82119410c9605ce07ec2192664c18c0d
Rbon <robinbisho@gmail.com>
2022-04-24 22:32:37 -0700

    Reimplemented incorrect letter checks

--- wordle_solver.hs
+++ wordle_solver.hs
@@ -8,11 +8,7 @@ import qualified WordList as W
 import Data.Function ( on )
 
 type Pair = (Char, Char)
-type Green = Char
-type Wrong = Char
-type Yellow = (Char, [Int])
-type Info = ([Green], [Yellow], [Wrong])
-
+type Info = (Matcher, [Char])
 type Matcher = String -> [Char] -> Bool
 
 -- | Compose two functions.
@@ -34,56 +30,88 @@ prompt text = do
 splitOnString :: String -> String -> [String]
 splitOnString delim = map T.unpack . T.splitOn (T.pack delim) . T.pack
 
-main :: IO ()
-main = interactiveStep "" W.allWords
+-- main :: IO ()
+-- main = interactiveStep "" W.allWords
 
 generatePairs :: String -> [Pair]
 generatePairs = uncurry zip . (\[x, y] -> (x,y)) . splitOnString " = "
 
-interactiveStep :: String -> [String] -> IO ()
-interactiveStep "" wordList = do
-    command <- prompt "guess: "
-    interactiveStep command wordList
+-- interactiveStep :: String -> [String] -> IO ()
+-- interactiveStep "" wordList = do
+--     command <- prompt "guess: "
+--     interactiveStep command wordList
 
-interactiveStep "quit" wordList = do
-    putStr ""
+-- interactiveStep "quit" wordList = do
+--     putStr ""
 
-interactiveStep command wordList = do
-    let newWordList = step command wordList
-    print newWordList
-    newCommand <- prompt "guess: "
-    interactiveStep newCommand newWordList
+-- interactiveStep command wordList = do
+--     let newWordList = step command wordList
+--     print newWordList
+--     newCommand <- prompt "guess: "
+--     interactiveStep newCommand newWordList
 
-generateGreens :: [Pair] -> [Char]
-generateGreens = map format where
-    format (char, color)
-        | color == 'g' = char
-        | otherwise    = '-'
 
-matchGreens :: String -> [Char] -> Bool
-matchGreens = and ... zipWith matcher where
-    matcher char '-' = True
-    matcher char green = char == green
 
-generateWrongs :: [Pair] -> [Char]
-generateWrongs = map format . filter pred where
-    pred (_, color) = color == 'w'
-    format (char, _) = char
+-- matchGreens :: String -> [Char] -> Bool
+-- matchGreens = and ... zipWith matcher where
+--     matcher char '-' = True
+--     matcher char green = char == green
 
-matchWrongs :: String -> [Char] -> Bool
-matchWrongs = all . flip notElem
+-- generateWrongs :: [Pair] -> [Char]
+-- generateWrongs = map format . filter pred where
+--     pred (_, color) = color == 'w'
+--     format (char, _) = char
 
-generateWrongs' :: [Pair] -> [String]
-generateWrongs' pairs = map (propagateWrong (greens pairs)) (wrongs pairs) where
-    greens pairs = map fst (filter isGreen pairs)
-    wrongs pairs = map fst (filter isWrong pairs)
-    isGreen (_, color) = color == 'g'
-    isWrong (_, color) = color == 'w'
-    propagateWrong greens wrong = zipWith f (allWrong greens wrong) greens
-    f wrong '-' = wrong
-    f _      _  = '-'
-    allWrong greens wrong = replicate (length greens) wrong
+-- matchWrongs :: String -> [Char] -> Bool
+-- matchWrongs = all . flip notElem
 
+-- generateGreens :: [Pair] -> [Info]
+-- generateGreens pairs = [(matchAll, map format pairs)] where
+--     format (char, color)
+--         | color == 'g' = char
+--         | otherwise    = '-'
+
+
+
+-- this is in need of tidying
+-- generateWrongs :: [Pair] -> [String]
+-- generateWrongs pairs = map (propagateWrong (greens pairs)) (wrongs pairs) where
+--     greens pairs = map fst (filter isGreen pairs)
+--     wrongs pairs = map fst (filter isWrong pairs)
+--     isGreen (_, color) = color == 'g'
+--     isWrong (_, color) = color == 'w'
+--     propagateWrong greens wrong = zipWith f (allWrong greens wrong) greens
+--     f wrong '-' = wrong
+--     f _      _  = '-'
+--     allWrong greens wrong = replicate (length greens) wrong
+
+generateGuess :: String -> (String, String)
+generateGuess = (\[x, y] -> (x,y)) . splitOnString " = "
+
+generateGreens :: (String, String) -> [Info]
+generateGreens (word, result) = [(matchAll, zipWith f word result)] where
+    f char1 char2 = if char2 == 'g' then char1 else '-'
+
+generateWrongs :: (String, String) -> [Info]
+generateWrongs (word, result) = map (matchNone,) properWrongs where
+    wrongs = filter (/= '-') $ generateWrongs' (word, result)
+    unknowns = generateUnknowns (word, result)
+    properWrongs = map (propagateLetter unknowns) wrongs
+
+propagateLetter :: String -> Char -> String
+propagateLetter str char = map (f char) str where
+    f char1 char2 = if char2 /= '-' then char1 else '-'
+
+generateWrongs' :: (String, String) -> String
+generateWrongs' (word, result) = zipWith f word result where
+    f char1 char2 = if char2 == 'w' then char1 else '-'
+
+generateUnknowns :: (String, String) -> String
+generateUnknowns (word, result) = zipWith f word result where
+    f char1 char2 = if char2 /= 'g' then char1 else '-'
+
+generateInfo :: (String, String) -> [Info]
+generateInfo guess = generateGreens guess ++ generateWrongs guess
 
 
 -- "idiot = wygyw"
@@ -107,17 +135,20 @@ needsToBeMatched = zipWith f where
     f _    '-' = '-'
     f char  _  = char
 
-matchAtLestOne :: String -> [Char] -> Bool
-matchAtLestOne = or ... zipWith (==) `on` filter (/= '-')
+matchAny :: String -> [Char] -> Bool
+matchAny = or ... zipWith (==) `on` filter (/= '-')
 
 matchNone :: String -> [Char] -> Bool
-matchNone = not ... matchAtLestOne
+matchNone = not ... matchAny
 
-checkAgainst :: String -> Matcher -> [Char] -> Bool
-checkAgainst str matcher chrs = matcher (needsToBeMatched str chrs) chrs
+matchAll :: String -> [Char] -> Bool
+matchAll = and ... zipWith (==) `on` filter (/= '-')
 
--- matchAtLestOne :: String -> [Char] -> Bool
--- matchAtLestOne = or ... zipWith (==)
+checkAgainst :: String -> (Matcher, [Char]) -> Bool
+checkAgainst str (matcher, chrs) = matcher (needsToBeMatched str chrs) chrs
+
+checkAgainstAll :: String -> [(Matcher, [Char])] -> Bool
+checkAgainstAll = all . checkAgainst
 
 generateYellows :: [Pair] -> [(Char, [Int])]
 generateYellows pairs = map properYellow allYellows where
@@ -141,27 +172,26 @@ matchYellows = all . matchOneYellow where
     matchOneYellow str (char, ns) = any (g str char) ns
     g str char n = str !! n == char
 
-generateInfo :: String -> Info
-generateInfo command = (greens, yellows, wrongs) where
-    pairs = generatePairs command
-    greens = generateGreens pairs
-    yellows = generateYellows pairs
-    wrongs = generateWrongs pairs
+-- generateInfo command = (greens, yellows, wrongs) where
+--     pairs = generatePairs command
+--     greens = generateGreens pairs
+--     yellows = generateYellows pairs
+--     wrongs = generateWrongs pairs
 
-matchInfo :: Info -> String -> Bool
-matchInfo(greens, yellows, wrongs) str  =
-    matchesGreen && matchesYellow && matchesWrong where
-    matchesGreen = matchGreens str greens
-    matchesYellow = matchYellows str yellows
-    matchesWrong = matchWrongs str wrongs
+-- matchInfo :: Info -> String -> Bool
+-- matchInfo(greens, yellows, wrongs) str  =
+--     matchesGreen && matchesYellow && matchesWrong where
+--     matchesGreen = matchGreens str greens
+--     matchesYellow = matchYellows str yellows
+--     matchesWrong = matchWrongs str wrongs
 
-step :: String -> [String] -> [String]
-step command wordList = newWords where
-    info = generateInfo command
-    newWords = filterWords info wordList
+-- step :: String -> [String] -> [String]
+-- step command wordList = newWords where
+--     info = generateInfo command
+--     newWords = filterWords info wordList
 
-filterWords :: Info ->  [String] -> [String]
-filterWords = filter . matchInfo
+-- filterWords :: Info ->  [String] -> [String]
+-- filterWords = filter . matchInfo
 
 -- beyond here is deprecated stuff,
 -- using generated words instead of a master list
@@ -174,17 +204,17 @@ initPossibles = [
     "abcdefghijklmnopqrstuvwxyz" ,
     "abcdefghijklmnopqrstuvwxyz" ]
 
-initStep :: String -> [String]
-initStep command = newWords where
-    p = generatePairs command
-    info = generateInfo command
-    newPossibles = filterPossibleChars p initPossibles
-    newWords = generateWords info newPossibles
+-- initStep :: String -> [String]
+-- initStep command = newWords where
+--     p = generatePairs command
+--     info = generateInfo command
+--     newPossibles = filterPossibleChars p initPossibles
+--     newWords = generateWords info newPossibles
 
 filterPossibleChars :: [Pair] -> [String] -> [String]
 filterPossibleChars = zipWith filter1 where
     filter1 (char, 'g') _   = [char]
     filter1 (char, _  ) str = remove char str
 
-generateWords :: Info -> [String] -> [String]
-generateWords pairs = filterWords pairs . sequence
+-- generateWords :: Info -> [String] -> [String]
+-- generateWords pairs = filterWords pairs . sequence
